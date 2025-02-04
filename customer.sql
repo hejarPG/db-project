85a2e3cd5a0d92921c2e7e221c1c9a644de0b1bc
hejarPG <hejar.adak@gmail.com>
2025-02-04 13:15:11 +0330

    Implement customer entity

---
+++ customer.sql
@@ -0,0 +1,19 @@
+CREATE TYPE sex_enum AS ENUM('male', 'female');
+
+CREATE TABLE customer(
+    id              SERIAL          PRIMARY KEY,
+    first_name      VARCHAR(255)    NOT NULL,
+    last_name       VARCHAR(255)    NOT NULL,
+    phone_number    VARCHAR(20)     UNIQUE NOT NULL,
+    password        VARCHAR(72)     NOT NULL,
+    city_id         INT,
+    sex             sex_enum,
+    birthdate       DATETIME,
+    image_uri       TEXT,
+    is_active       BOOLEAN         DEFAULT TRUE,
+    created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (city_id) REFERENCES city(id) ON DELETE SET NULL
+);
+
